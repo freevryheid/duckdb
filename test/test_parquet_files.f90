abc9ee395f7f4f656ac2b35f05621b05f1ec3dfd
Ludovico Nicotina <ludovico.nicotina@gmail.com>
2023-06-27 20:44:41 +0000

    update parquet test

--- test/test_parquet_files.f90
+++ test/test_parquet_files.f90
@@ -23,7 +23,10 @@ end subroutine collect_parquet_files
     type(duckdb_database) :: db
     type(duckdb_connection) :: con
     type(duckdb_result) :: result = duckdb_result()
-    integer :: i, j, nc, nr
+    type(duckdb_data_chunk) :: chunk
+    type(duckdb_vector) :: vector
+    type(duckdb_logical_type) :: type
+    integer :: i, j, nc, nr, row_count, col_count
 
     ! Open data in in-memory mode
     call check(error, duckdb_open("", db) == duckdbsuccess)
@@ -60,10 +63,18 @@ end subroutine collect_parquet_files
     call check(error, duckdb_column_type(result, 1) == duckdb_type_varchar)
     if (allocated(error)) return
 
-    ! do i = 1,
-    ! Retrieving the table doesn't work yet.
-    ! print*, duckdb_result_chunk_count(result)
-    ! duckdb_result_get_chunk(result, )
+
+    do i = 0, duckdb_result_chunk_count(result)-1
+      chunk = duckdb_result_get_chunk(result, i)
+      col_count = duckdb_data_chunk_get_column_count(chunk)
+      row_count = duckdb_data_chunk_get_size(chunk)
+      print*,  col_count, row_count
+      do j = 0, col_count - 1
+        vector = duckdb_data_chunk_get_vector(chunk, j)
+        type = duckdb_vector_get_column_type(vector)
+        print*, i, j, duckdb_get_type_id(type)
+      enddo
+    enddo
 
     ! Write the out the same table to a different parquet file
     call check(error, duckdb_query( &
@@ -72,23 +83,21 @@ end subroutine collect_parquet_files
       result) == duckdbsuccess)
     if (allocated(error)) return
 
-    ! NOTE: parquet files moved to data directory
-
     !! Later get the schema from the original and the resulting parquet and compare them
     !! But it doesn't work until when we can't read the result.
-    ! ! get the parquet schema
-    ! call check(error, duckdb_query(                                           &
-    !     con,                                                                  &
-    !     "SELECT * FROM parquet_schema('test.parquet');",                      &
-    !     result) == duckdbsuccess)
-    ! if (allocated(error)) return
+    ! get the parquet schema
+    call check(error, duckdb_query(                                           &
+        con,                                                                  &
+        "SELECT * FROM parquet_schema('data/test.parquet');",                      &
+        result) == duckdbsuccess)
+    if (allocated(error)) return
 
-    ! ! get the parquet schema
-    ! call check(error, duckdb_query(                                           &
-    !     con,                                                                  &
-    !     "SELECT * FROM parquet_schema('result.parquet');",                      &
-    !     result) == duckdbsuccess)
-    ! if (allocated(error)) return
+    ! get the parquet schema
+    call check(error, duckdb_query(                                           &
+        con,                                                                  &
+        "SELECT * FROM parquet_schema('data/result.parquet');",                      &
+        result) == duckdbsuccess)
+    if (allocated(error)) return
 
     call duckdb_destroy_result(result)
     call duckdb_disconnect(con)
